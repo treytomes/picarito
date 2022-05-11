4f8d3a4a3db88deef7039ab4ccf21952acc8b2f2
Trey Tomes <trey.tomes@gmail.com>
2022-05-11 07:06:18 -0500

    Reinstated distortion.

--- src/framework/canvas.fs
+++ src/framework/canvas.fs
@@ -3,36 +3,36 @@ varying vec2 v_texcoord;
 uniform sampler2D u_texture;
 uniform float u_time;
 
-vec2 distort(vec2 pos) {
-	float distortion = 0.01;
-	pos -= vec2(0.5, 0.5);
-	pos *= vec2(pow(length(pos), distortion));
-	pos += vec2(0.5, 0.5);
-	return pos;
+// Vignette inspired by: https://babylonjs.medium.com/retro-crt-shader-a-post-processing-effect-study-1cb3f783afbc
+
+vec2 curveRemapUV(vec2 pos, vec2 curvature) {
+    // As we near the edge of our screen apply greater distortion using a cubic function.
+    pos = pos * 2.0 - 1.0;
+    vec2 offset = abs(pos.yx) / vec2(curvature.x, curvature.y);
+    pos = pos + pos * offset * offset;
+    pos = pos * 0.5 + 0.5;
+    return pos;
 }
 
-void main() {
-	vec2 pos = v_texcoord;
+vec4 vignetteIntensity(vec2 pos, vec2 resolution, float opacity, float roundness) {
+	float intensity = pos.x * pos.y * (1.0 - pos.x) * (1.0 - pos.y);
+	return vec4(vec3(clamp(pow((resolution.x / roundness) * intensity, opacity), 0.0, 1.0)), 1.0);
+}
 
-	// Barrel Distortion
-	//pos = distort(pos);
-
-	vec4 color = texture2D(u_texture, v_texcoord);
-
-	// Scanlines
-
+vec4 scanlines(vec2 pos, vec4 color) {
 	color *= sign(sin(pos.y * 3000.0));
-	//color -= abs(sin(pos.y * 100.0 + u_time * 5.0)) * 0.08; // (1)
 	color.a = 1.0;
 	
 
 	color -= abs(sin(pos.y * 100.0 + u_time * 5.0)) * 0.08;
 	color.a = 1.0;
 
-	// Bloom
+	return color;
+}
 
-	float dx = 1.0 / 256.0 / 2.0;
-	float dy = 1.0 / 224.0 / 2.0;
+vec4 bloom(vec4 color, vec2 pos, float glowFactor, float originWeight, vec2 screenResolution) {
+	float dx = 1.0 / screenResolution.x / 2.0;
+	float dy = 1.0 / screenResolution.y / 2.0;
 	vec4 color0 = texture2D(u_texture, vec2(pos.x - dx, pos.y - dy));
 	vec4 color1 = texture2D(u_texture, vec2(pos.x +0.0, pos.y - dy));
 	vec4 color2 = texture2D(u_texture, vec2(pos.x + dx, pos.y - dy));
@@ -43,10 +43,28 @@ void main() {
 	vec4 color7 = texture2D(u_texture, vec2(pos.x +0.0, pos.y + dy));
 	vec4 color8 = texture2D(u_texture, vec2(pos.x + dx, pos.y + dy));
 
+	color = (color + color0 + color1 + color2 + color3 + color4 + color5 + color6 + color7 + color8) / 9.0 * (1.0 - originWeight) + color * (originWeight + glowFactor);
+	return color;
+}
+
+void main() {
+	vec2 curvature = vec2(4.0, 4.0);
+	vec2 screenResolution = vec2(256.0, 224.0);
 	float glowFactor = 0.9;
 	float originWeight = 0.1;
+	float vignetteOpacity = 0.6;
+	float vignetteRoundness = 2.0;
+	vec2 pos = v_texcoord;
+	pos = curveRemapUV(pos, curvature);
 
-	color = (color + color0 + color1 + color2 + color3 + color4 + color5 + color6 + color7 + color8) / 9.0 * (1.0 - originWeight) + color * (originWeight + glowFactor);
-
-	gl_FragColor = color;
+	vec4 color = texture2D(u_texture, pos);
+	color = scanlines(pos, color);
+	color = bloom(color, pos, glowFactor, originWeight, screenResolution);
+	color *= vignetteIntensity(pos, vec2(256, 224), vignetteOpacity, vignetteRoundness);
+	
+    if (pos.x < 0.0 || pos.y < 0.0 || pos.x > 1.0 || pos.y > 1.0){
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    } else {
+        gl_FragColor = color;
+    }
 }
